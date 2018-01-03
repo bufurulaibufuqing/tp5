1a0ade67411580991e0b51c7ee0cb235639e233b
wangxuegang <wangxuegang@dycd.com>
2018-01-03 18:26:07 +0800

    oauth

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,8 +1,25 @@
 <?php
 namespace app\index\controller;
 
-class Index{
+use app\common\core\BaseController;
+
+class Index extends BaseController{
+
+//    public function __construct(){
+//        parent::__construct();
+//    }
+
     public function index(){
-        return view();
+        $info['param'] = $this->request->param();
+        $info['check_value'] = ['app_key','app_secret'];
+        //值不能为空
+        $check_result = checkValueEmpty($info['check_value'],$info['param']);
+        if($check_result['code'] != 1){
+            return $this->output($check_result);
+        }
+        $data['code']='0000000';
+        $data['message']='成功';
+        $data['data']=model('DeveloperUser')->getUserData($info['param']['app_key'],$info['param']['app_secret']);
+        return $this->output($data);
     }
 }
